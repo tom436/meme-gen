d54e3cf5c92b90419157b6df437a35f6e949573b
tom temkin <tomtemkin14@gmail.com>
2020-04-13 20:06:15 +0300

    newst

--- js/meme-util-controller.js
+++ js/meme-util-controller.js
@@ -4,4 +4,7 @@
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
   }
-  
+  function openNav() {
+
+    document.body.classList.toggle("menu-open")
+}
